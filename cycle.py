92c560a2d61b58ecf623ead1cb7fb2ff72176d33
Sam Morello <you@example.com>
2025-03-05 17:51:28 -0700

    filtered data, beginning eda

--- cycle.py
+++ cycle.py
@@ -5,11 +5,12 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('/Users/sammorello/Projects/CycleTracking/naturalcyclesdata/Daily Entries.csv')
 
 # display first few rows
+print("First few rows of data:")
 print(df.head())
 
 # display and drop empty columns
 empty_cols = df.columns[df.isna().all()]
-print("completely empty columns:")
+print("Completely empty columns:")
 print(empty_cols)
 
 # drop columns where all values are NaN
@@ -21,7 +22,7 @@ df = df.dropna(axis=1, how="all")
 df.drop(columns=["Skipped", "Had sex", "Sex Type", "Libido"],  inplace=True)
 
 default_values = {
-    "Temperature": "emp Not Recorded",
+    "Temperature": "Temp Not Recorded",
     "Menstruation": "Not Menstruating",
     "LH test": "LH Not Tested",
     "Had sex": "No",
@@ -31,4 +32,32 @@ default_values = {
 }
 df.fillna(default_values, inplace=True)
 
+# remove entries
+df["Data Flag"] = df["Data Flag"].str.replace(r"\b(SEX_MASTURBATION|SEX_ORAL)\b,?\s*", "", regex=True)
+df["Data Flag"] = df["Data Flag"].replace("", "No Data")
+
+# convert to datetime format
+df["Date"] = pd.to_datetime(df["Date"], errors="coerce")
+
+# print specific day
+print(df[df["Date"] == "2023-12-17"])
+
+# checking data flag output
+print(df.loc[df["Date"] == "2023-12-17", "Data Flag"])
+
+# filter data to 1 year of entries
+start_date = "2023-10-11"
+end_date = "2024-10-11"
+df_filtered = df[(df["Date"] >= start_date) & (df["Date"] <= end_date)]
+
+# check filtered range
+print(df_filtered["Date"].min(), df_filtered["Date"].max())  
+
+print("Cleaned data:")
 print(df)
+
+# EDA
+
+# number of days menstruating
+print(df_filtered["Menstruation"].value_counts())
+
